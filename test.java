b246c97be0bb4b238508c3724986bf44a1a054d2
Qrutz <40356149+Qrutz@users.noreply.github.com>
2022-08-29 16:49:00 +0200

    Update test.java

--- test.java
+++ test.java
@@ -274,3 +274,40 @@ class Concat {
         return xd;
     }
 }
+
+
+class PascalsT {
+
+    public static List<List<Integer>> pascals(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        if (numRows == 0)
+            return triangle;
+
+        List<Integer> first_row = new ArrayList<>();
+        first_row.add(1);
+        triangle.add(first_row);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prev_row = triangle.get(i - 1);
+            List<Integer> current_row = new ArrayList<>();
+            current_row.add(1);
+
+            for (int j = 1; j < i; j++) {
+                
+                current_row.add((prev_row.get(j - 1)) + prev_row.get(j));
+            }
+
+            current_row.add(1);
+            triangle.add(current_row);
+
+        }
+        return triangle;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pascals(5));
+
+    }
+}
